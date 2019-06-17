42b419dac9ad3fa124e8941b353a9fb58c5fc373
tavilad <pintiliciucoctavian@gmail.com>
2019-06-17 14:37:12 +0300

    Solved Hello World,Pangram and TwoFer exercises

---
+++ pangram/Pangram.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return false;
+        }
+
+        bool hasPuntuation = input[input.Length-1].Equals(".");
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        int count = 0;
+
+        foreach(char c in alphabet)
+        {
+            foreach (char lit in input.ToLower())
+            {
+                if (c == lit)
+                {
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        if(count == 26 && !hasPuntuation)
+        {
+            return true;
+        }
+        else
+        {
+            if(count==27 && hasPuntuation)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ two-fer/TwoFer.cs
@@ -0,0 +1,12 @@
+﻿public static class TwoFer
+{
+    public static string Speak(string input="")
+    {
+        if (input.Equals(string.Empty))
+        {
+            return "One for you, one for me.";
+        }
+
+        return "One for " + input + ", one for me.";
+    }
+}
